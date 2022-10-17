af6d032dcbe3aff397e405e831c80b8868eb0f9d
laurabrovkina <laurabrovkina@gmail.com>
2022-10-17 14:43:48 +1300

    Functional actions

---
+++ MeetupAPI/Controllers/ConfigController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MeetupAPI.Controllers
+{
+    [Route("config")]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpOptions("reload")]
+        public ActionResult Reload()
+        {
+            try
+            {
+                ((IConfigurationRoot)_configuration).Reload();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }
+        }
+
+    }
+}
