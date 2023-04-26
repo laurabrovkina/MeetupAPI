200437a4cdaad63c47992f3ee92897863ba1c570
laurabrovkina <laurabrovkina@gmail.com>
2023-04-26 23:07:49 +1200

    Example an exception middleware

---
+++ MeetupAPI/Validators/ValidationExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace MeetupAPI.Validators
+{
+    public class ValidationExceptionMiddleware
+    {
+        private readonly ProblemDetailsFactory _problemDetailsFactory;
+
+        public ValidationExceptionMiddleware(ProblemDetailsFactory problemDetailsFactory)
+        {
+            _problemDetailsFactory = problemDetailsFactory;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (ValidationException exception)
+            {
+                // set http status code and content type
+                var problem = _problemDetailsFactory.CreateProblemDetails(
+                    context,
+                    statusCode: StatusCodes.Status400BadRequest,
+                    detail: exception.Message,
+                    title: "Bad Request",
+                    type: $"{exception.GetType()}");
+
+                // writes / returns error model to the response
+                await context.Response.WriteAsync(
+                    JsonConvert.SerializeObject(problem));
+            }
+        }
+    }
+}
