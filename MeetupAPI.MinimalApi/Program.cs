ad4a9acc52b43314123d917ff67cdc849fd39270
Larisa <laurabrovkina@gmail.com>
2023-07-11 15:09:51 +1200

    Update Program.cs

--- MeetupAPI.MinimalApi/Program.cs
+++ MeetupAPI.MinimalApi/Program.cs
@@ -13,7 +13,12 @@ var config = builder.Configuration;
 builder.Services.AddFastEndpoints();
 builder.Services.AddSwaggerDoc();
 
-builder.Services.AddDbContext<MeetupContext>(option => option.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MeetupDb;Trusted_Connection=True;"));
+// .AddDbContext is scoped - which means that for each HTTP request, we have a single DB context and 
+// this could be a problem because when we run multiple resolvers in parallel because the context that we share in this HTTP request
+// is not thread-safe which leads to nasty exceptions happening from time to time
+// .AddDbContextPool is doing the pooling without using the factory. We still have one single instance per HTTP request or per service scope
+// So this instance is retrieved from the pool. We reuse the DB context and have a better memory footprint but we have the same usage as we had before
+builder.Services.AddDbContextPool<MeetupContext>(option => option.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MeetupDb;Trusted_Connection=True;"));
 
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 builder.Services.AddSingleton<IAccountRepository, AccountRepository>();
